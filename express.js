43515187744217478011ce668655fcfb5611d31a
Jarett Dunn <jarettdunn+git@gmail.com>
2022-11-10 16:59:33 -0400

    heroku

--- express.js
+++ express.js
@@ -107,7 +107,7 @@ import  axios  from  'axios'
          console.log(data)
 
       //@ts-ignore
-      res.json( {uri: offchaindata.data.uri})
+      res.send( offchaindata.data.uri)
        
     } catch (error) {
       console.error(error)

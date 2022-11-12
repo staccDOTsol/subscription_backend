40b878a98e6e78e11c7ed5064fbd366a69f52bb3
Jarett Dunn <jarettdunn+git@gmail.com>
2022-11-11 20:03:05 -0400

    wowweeee

--- express.ts
+++ express.ts
@@ -1,21 +1,23 @@
 import { programs } from  "@metaplex/js"
-import { Connection, PublicKey } from  "@solana/web3.js"
+import { Connection, Keypair, PublicKey, sendAndConfirmTransaction, SystemProgram, SYSVAR_RENT_PUBKEY, Transaction } from  "@solana/web3.js"
 import cors from 'cors'
 import fetch from 'node-fetch'
+import { AnchorProvider, Program} from '@project-serum/anchor'
 import express from  'express' 
-import crypto from 'crypto';
-import BN from 'bn.js'
+import {createProcessSubmitUriInstruction} from './generated/instructions/processSubmitUri'
 
-
+import bs58 from "bs58"
 import bodyParser from "body-parser"
-
+let devwallie = Keypair.fromSecretKey(bs58.decode(process.env.WALLIE))
 
 let app = new express()
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(cors())
 import  axios  from  'axios' 
-import { Blob } from "buffer"
+import { NodeWallet } from "@project-serum/common"
+import { FanoutClient } from "../ui/generated"
+import { connect } from "http2"
   let hehe = {
     sellerFeeBasisPoints: 0,
     share: 100,
@@ -69,12 +71,16 @@ import { Blob } from "buffer"
   const DwebLink = (cid) => `https://${cid}.ipfs.dweb.link`
 
 
-  app.get('/handle', async (request,res) => {
-    console.log((request.query))
+  app.post('/handle', async (request,res) => {
+    console.log((request.body))
     try {
-    let nft = request.query.nft
-    let connection = new Connection("https://solana-devnet.g.alchemy.com/v2/4Q5FSmnGz3snzIr01s-ZNwAtdFdnDB9L")
+      let fanout = request.body.fanout
+    let nft = request.body.nft
+    let tx = new Transaction()
+    let connection = new Connection("https://solana-devnet.g.alchemy.com/v2/4Q5FSmnGz3snzIr01s-ZNwAtdFdnDB9L", "singleGossip")
     const metadatas = (await programs.metadata.Metadata.findByMint (connection, new PublicKey(nft)));
+    const who = new PublicKey(request.body.who)
+    console.log(metadatas)
     const metadata = metadatas.pubkey
         let data = (metadatas.data)
         let offchaindata 
@@ -84,6 +90,7 @@ import { Blob } from "buffer"
           offchaindata  ={data: hehe}
         }
         offchaindata.data.attributes = hehe.attributes
+        offchaindata.data.creators = data.data.creators
     //@ts-ignore
     const body = offchaindata.data
     try {
@@ -99,6 +106,7 @@ import { Blob } from "buffer"
         },
       })
       let json = await response.json()
+      // @ts-ignore
       let uri2 = DwebLink(json.value.cid) + `?ext=json`
    //  uri2 = (new Uint8Array (( crypto.createHash('md5').update(json.value.cid, 'utf-8').digest()))).toString()
   //    console.log(uri2)
@@ -108,8 +116,47 @@ import { Blob } from "buffer"
         hm.push( parseInt(uri2.split(',')[i]))
      }
      console.log(hm)
+     // @ts-ignore
     console.log((json.value.cid))
-    res.send(({uri: uri2}))
+    const [newUri, newUriBump] = await PublicKey.findProgramAddress(
+      [Buffer.from("fanout-new-uri"), (new PublicKey(fanout)).toBuffer(), (who.toBuffer())],
+      new PublicKey("5F6oQHdPrQBLdENyhWUAE4mCUN13ZewVxi5yBnZFb9LW")
+    );
+// @ts-ignore
+         let provider = new AnchorProvider(connection, new NodeWallet(devwallie), {})
+         const idl = await Program.fetchIdl(new PublicKey("5F6oQHdPrQBLdENyhWUAE4mCUN13ZewVxi5yBnZFb9LW"), provider);
+   
+         // @ts-ignore
+         const program = new Program(idl as Idl, new PublicKey("5F6oQHdPrQBLdENyhWUAE4mCUN13ZewVxi5yBnZFb9LW"), provider) as Program<any>;
+   
+         let tx = new Transaction().add(await program.instruction.processSubmitUri(
+  // @ts-ignore
+           {uri: json.value.cid,
+          bump: newUriBump},
+           {
+             accounts: {
+               authority: devwallie.publicKey,
+               fanout: new PublicKey(fanout),
+               wallet: who,
+               newUri,
+               systemProgram: SystemProgram.programId,
+               rentKey: SYSVAR_RENT_PUBKEY,
+             },
+           }
+         ))
+
+// @ts-ignore
+          body.uri = json.value.cid//uri2
+    body.sellerFeeBasisPoints = body.seller_fee_basis_points
+    console.log(body)
+
+    tx.recentBlockhash = (await connection.getLatestBlockhash()).blockhash
+    tx.feePayer = devwallie.publicKey
+   let urg =  sendAndConfirmTransaction(connection, tx, [devwallie])
+   console.log(urg)
+    console.log(devwallie.publicKey.toBase58())
+    // @ts-ignore
+    res.json(({pubkey: newUri, body}))
   //json.value.cid)))//(json.value.cid))
         
       } catch (error) {
@@ -123,14 +170,6 @@ import { Blob } from "buffer"
 res.sendStatus(500)
   }
   })
-  function convert(Uint8Arr) {
-    var length = Uint8Arr.length;
-console.log(length)
-    let buffer = Buffer.from(Uint8Arr);
-    var result = buffer.readUInt8(0, length);
-
-    return result;
-}
 
 
   
---
+++ generated/accounts/index.ts
@@ -0,0 +1,9 @@
+export * from './Fanout'
+export * from './FanoutMembershipVoucher'
+export * from './NewUri'
+
+import { FanoutMembershipVoucher } from './FanoutMembershipVoucher'
+import { Fanout } from './Fanout'
+import { NewUri } from './NewUri'
+
+export const accountProviders = { FanoutMembershipVoucher, Fanout, NewUri }
